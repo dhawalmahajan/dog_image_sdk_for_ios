54197006ac282c3df638c11a3a2b86f49eb972a5
Dhawal Mahajan <dhawalmahajan@users.noreply.github.com>
2024-07-30 16:49:02 +0530

    Add files via upload

---
+++ Dogram/Features/DogImage/Views/DogImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DogImageCollectionViewCell.swift
+//  Dogram
+//
+//  Created by Dhawal Mahajan on 30/07/24.
+//
+
+import UIKit
+
+class DogImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "DogImageCollectionViewCell"
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
